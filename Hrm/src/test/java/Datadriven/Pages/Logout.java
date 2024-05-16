d1b84c91e3c98a8095d678da9d20b13a689578cd
KavyashreeBaindoor <kavyashreebaindoor@gmail.com>
2024-05-16 05:39:12 -0700

    added login and logout pages

--- Hrm/src/test/java/Datadriven/Pages/Logout.java
+++ Hrm/src/test/java/Datadriven/Pages/Logout.java
@@ -1,5 +1,23 @@
 package Datadriven.Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Logout {
+	
+private WebDriver driver;
+	
+	Logout(WebDriver d){
+		this.driver=d;
+	}
+	
+	private By dropDownIcon=By.xpath("//span[@class='oxd-userdropdown-tab']/img[@alt='profile picture']");
+	private By logoutBtn=By.xpath("//ul[@class='oxd-dropdown-menu']/li[4]/a");
+	
+	public void logout() {
+		driver.findElement(dropDownIcon).click();
+		driver.findElement(dropDownIcon).click();
+	}
+	
 
 }
